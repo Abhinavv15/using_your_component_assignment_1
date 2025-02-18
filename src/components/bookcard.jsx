ff4fb01f88eeac69db583714d6d82c3ca642e4ec
Abhinavv15 <abhinavdhanesh15@gmail.com>
2025-02-18 09:44:40 +0530

    Completed the assignment

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,21 @@
 // write the book component code here
+
+const BookCard = ({book}) =>{
+    return (
+        <div style={{
+            border: '2px solid red',
+            textAlign: 'center',
+            padding: '10px',
+            margin: '10px',
+            width: '200px'
+        }}>
+            <img src={book.image} alt={book.name} style={{ width: '100%', borderRadius: '5px' }} />
+            <h3>{book.name}</h3>
+            <p>{book.genre}</p>
+            <p>{book.author}</p>
+        </div>
+    )
+}
+
+
+export default BookCard
